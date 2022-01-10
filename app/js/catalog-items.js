6ef41b6244f50c2bb5ebfb67506f7ac4b92d90e5
Peresvet <Koltyrinpk@gmail.com>
2022-01-10 16:19:38 +0400

    работа над корзиной

--- app/js/catalog-items.js
+++ app/js/catalog-items.js
@@ -1,7 +1,5 @@
 "use stirct"
 
-
-
 //--------------------------------------------------- работа бургера, меню
 let burger = document.querySelector('.burger');
 let activeMenu = document.querySelector(".bottom-menu");
@@ -30,47 +28,6 @@ $(function() {
     });
 });
 
-// -----------------------------------------------------------------------Фильтр
-let priceSlider = document.querySelector('.price-filter');
-noUiSlider.create(priceSlider, {
-    start: [3000, 28000],
-    margin: 9000,
-    
-    tooltips: [wNumb({decimals: 0, suffix: ' ₽'}), wNumb({decimals: 0, suffix: ' ₽'})],
-    connect: true,
-    range: {
-        'min': 3000,
-        'max': 35000,
-    }
-});
-// --------------------------------сворачивание контента
-let filterBody = document.getElementsByClassName('filter__body');
-let arrow = document.getElementsByClassName('filter-title__svg');
-let dropdown = document.getElementsByClassName('section-filter__body');
-
-for(let i = 0; i < arrow.length; i++){
-	arrow[i].addEventListener("click", function() {
-    	dropdown[i].classList.toggle("disp-none");
-		arrow[i].classList.toggle("rotate-180");
-  	});
-};
-
-// -----------------------------------Адаптив фильтра
-let filtrBtn = document.querySelector('.filter-btn');
-let filter = document.querySelector('.filter');
-
-filtrBtn.onclick = () =>{
-	
-    filter.classList.toggle('adaptive-filter');
-	filtrBtn.classList.toggle('filter-btn__active');
-
-	if(filtrBtn.classList.contains('filter-btn__active')){
-		document.getElementById('filter-btn').innerHTML = 'Применить';
-	}else{
-		document.getElementById('filter-btn').innerHTML = 'Фильтры';
-	}
-}
-
 //-----------------------------------------------------------счетчик товаров-------------------------------------------------
 window.addEventListener('click', (event)=>{
     if(event.target.dataset.action == "plus" || event.target.dataset.action == "minus" ){
@@ -87,6 +44,21 @@ window.addEventListener('click', (event)=>{
                 counter.textContent = --counter.textContent;
             }
         }
+    }
+});
+window.addEventListener('click', (event) =>{
+    // проверяем, что клик был совершен по кнопке
+    if(event.target.hasAttribute('data-cart')){
+        // находим карточку товара, внутри которой был совершен клик
+        const card = event.target.closest('.mini-catalog__item');
+        // собираем данные с этого товара
+        const productInfo = {
+            id: card.dataset.id, // получаем data-id="01"
+            imgSrc: card.querySelector('.mini-catalog__img').getAttribute('src'),
+            title: card.querySelector('.mini-catalog__text').textContent,
+            price: card.querySelector('.mini-catalog__price').textContent,
+            counter: card.querySelector('[data-counter]').textContent,
+        };
     }
 });
 
